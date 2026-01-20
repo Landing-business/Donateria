f40f53f4fa2ddeee7ca7a4088a12e86a8032c407
Bissness <artemmiek@gmail.com>
2026-01-20 20:25:19 +0300

    Delete scripts directory

--- scripts/dishes-select.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('dishes-select').addEventListener('change', function () {
-  const targetId = this.value;
-  if (targetId) {
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-});
